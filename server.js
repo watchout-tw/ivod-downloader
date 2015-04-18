91e17d5dbcb3a8544109af4d06be32af9a647993
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2015-04-18 16:45:08 +0800

    Add @buttom and @shulusama's design

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use (express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/' ,function (req, res) {
